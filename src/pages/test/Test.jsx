d18883c1978be2d643521c1e44a28bbb038003be
anxiubin <tnqls0120@naver.com>
2020-10-20 18:41:47 +0900

    ⚡ babel-plugin-module-resolver 적용

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useDispatch } from "react-redux"
-import useTodos from "../../modules/todos/hooks/useTodos"
-import { getTodos } from "../../modules/todos/"
+import useTodos from "@modules/todos/hooks/useTodos"
+import { getTodos } from "@modules/todos/"
 
 export default function Test() {
 	const dispatch = useDispatch()
